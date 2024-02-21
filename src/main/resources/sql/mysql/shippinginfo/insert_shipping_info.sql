d16167186b52859b870c9f56f4b1f4d96b5cea33
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-21 18:17:24 +0900

    chore: Changed to insert initial data (local only)

--- src/main/resources/sql/mysql/shippinginfo/insert_shipping_info.sql
+++ src/main/resources/sql/mysql/shippinginfo/insert_shipping_info.sql
@@ -1,5 +1,5 @@
 -- SHIPPING_INFOS-data.sql
-INSERT INTO SHIPPING_INFOS (ID, MEMBER_EMAIL, RECEIVER, MAIN_ADDRESS, SUB_ADDRESS, PHONE_NUMBER, CREATED_DATE,
+INSERT INTO SHIPPING_INFOS (ID, MEMBER_NO, RECEIVER, MAIN_ADDRESS, SUB_ADDRESS, PHONE_NUMBER, CREATED_DATE,
                             UPDATED_DATE)
-VALUES (1, 'qkrtkdwns3410@naver.com', 'John Doe', '123 Main St', 'Apt 4B', 1234567890, NOW(), NOW()),
-       (2, 'qkrtkdwns34102@naver.com', 'Jane Doe', '456 Elm St', 'Apt 2A', 9876543210, NOW(), NOW());
+VALUES (1, 1, 'John Doe', '123 Main St', 'Apt 4B', 1234567890, NOW(), NOW()),
+       (2, 2, 'Jane Doe', '456 Elm St', 'Apt 2A', 9876543210, NOW(), NOW());
