cf9418d1c55eabe0ad84ad97b4383a2ab51d70ca
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-22 14:27:34 +0900

    fix: Add List NPE check

--- src/main/java/com/psj/itembrowser/product/domain/dto/response/ProductResponseDTO.java
+++ src/main/java/com/psj/itembrowser/product/domain/dto/response/ProductResponseDTO.java
@@ -1,12 +1,16 @@
 package com.psj.itembrowser.product.domain.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.psj.itembrowser.product.domain.entity.ProductEntity;
 import com.psj.itembrowser.product.domain.vo.Product;
 import com.psj.itembrowser.product.domain.vo.ProductStatus;
+import com.psj.itembrowser.security.common.exception.ErrorCode;
+import com.psj.itembrowser.security.common.exception.NotFoundException;
 
 import lombok.Builder;
 import lombok.Data;
@@ -14,46 +18,52 @@ import lombok.Data;
 @Data
 @Builder
 public class ProductResponseDTO {
-
+	
 	private Long id;
-
+	
 	private String name;
-
+	
 	private Integer category;
-
+	
 	private String detail;
-
+	
 	private ProductStatus status;
-
+	
 	private Integer quantity;
-
+	
 	private Integer unitPrice;
-
+	
 	private String sellerId;
-
+	
 	private LocalDateTime sellStartDatetime;
-
+	
 	private LocalDateTime sellEndDatetime;
-
+	
 	private String displayName;
-
+	
 	private String brand;
-
+	
 	private String deliveryMethod;
-
+	
 	private Integer deliveryDefaultFee;
-
+	
 	private Integer freeShipOverAmount;
-
+	
 	private String returnCenterCode;
-
+	
 	private List<ProductImageResponseDTO> productImages;
-
+	
 	public static ProductResponseDTO from(Product product) {
 		if (product == null) {
-			return null;
+			throw new NotFoundException(ErrorCode.PRODUCT_NOT_FOUND);
 		}
-
+		
+		List<ProductImageResponseDTO> productImages = Optional.ofNullable(product.getProductImages())
+			.map(images -> images.stream()
+				.map(ProductImageResponseDTO::from)
+				.collect(Collectors.toUnmodifiableList())
+			).orElse(Collections.emptyList());
+		
 		return ProductResponseDTO.builder()
 			.id(product.getId())
 			.name(product.getName())
@@ -71,19 +81,21 @@ public class ProductResponseDTO {
 			.deliveryDefaultFee(product.getDeliveryDefaultFee())
 			.freeShipOverAmount(product.getFreeShipOverAmount())
 			.returnCenterCode(product.getReturnCenterCode())
-			.productImages(product.getProductImages()
-				.stream()
-				.map(ProductImageResponseDTO::from)
-				.collect(Collectors.toList())
-			)
+			.productImages(productImages)
 			.build();
 	}
-
+	
 	public static ProductResponseDTO from(ProductEntity entity) {
 		if (entity == null) {
-			return null;
+			throw new NotFoundException(ErrorCode.PRODUCT_NOT_FOUND);
 		}
-
+		
+		List<ProductImageResponseDTO> productImages = Optional.ofNullable(entity.getProductImages())
+			.map(images -> images.stream()
+				.map(ProductImageResponseDTO::from)
+				.collect(Collectors.toUnmodifiableList())
+			).orElse(Collections.emptyList());
+		
 		return ProductResponseDTO.builder()
 			.id(entity.getId())
 			.name(entity.getName())
@@ -101,10 +113,7 @@ public class ProductResponseDTO {
 			.deliveryDefaultFee(entity.getDeliveryDefaultFee())
 			.freeShipOverAmount(entity.getFreeShipOverAmount())
 			.returnCenterCode(entity.getReturnCenterCode())
-			.productImages(entity.getProductImages().stream()
-				.map(ProductImageResponseDTO::from)
-				.collect(Collectors.toList())
-			)
+			.productImages(productImages)
 			.build();
 	}
 }
