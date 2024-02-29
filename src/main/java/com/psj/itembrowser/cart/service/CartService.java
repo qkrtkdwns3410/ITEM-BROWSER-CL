e66dc07ddefebafc627e5ffd24e6596083e5f778
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-29 13:03:48 +0900

    refactor: 삭제. 트랜잭션 전파 기본전략을 굳이 명시할 이유가 없어보임

--- src/main/java/com/psj/itembrowser/cart/service/CartService.java
+++ src/main/java/com/psj/itembrowser/cart/service/CartService.java
@@ -1,7 +1,6 @@
 package com.psj.itembrowser.cart.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.psj.itembrowser.cart.domain.dto.request.CartProductDeleteRequestDTO;
@@ -29,52 +28,52 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CartService {
-
+	
 	private final CartPersistence cartPersistence;
 	private final CartMapper cartMapper;
 	
 	public CartResponseDTO getCart(String userEmail) {
 		return cartPersistence.getCart(userEmail);
 	}
-
+	
 	public CartResponseDTO getCart(Long cartId) {
 		return cartPersistence.getCart(cartId);
 	}
-
+	
 	public void addCart(@NonNull String userId) {
 		cartPersistence.addCart(userId);
 	}
-
-	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+	
+	@Transactional(readOnly = false)
 	public void addCartProduct(CartProductRequestDTO requestDTO) {
 		CartResponseDTO cart = null;
-
+		
 		try {
 			cart = getCart(requestDTO.getUserId());
 		} catch (NotFoundException e) {
 			log.info("cart not found, add cart");
 		}
-
+		
 		if (cart == null) {
 			addCart(requestDTO.getUserId());
 		}
-
+		
 		CartProductRelation findCartProduct = cartMapper.getCartProductRelation(requestDTO.getCartId(), requestDTO.getProductId());
-
+		
 		if (findCartProduct != null) {
 			findCartProduct.addProductQuantity(requestDTO.getQuantity());
 			cartPersistence.modifyCartProduct(CartProductUpdateRequestDTO.from(findCartProduct));
 			return;
 		}
-
+		
 		cartPersistence.insertCartProduct(requestDTO);
 	}
-
+	
 	@Transactional(readOnly = false)
 	public void modifyCartProduct(CartProductUpdateRequestDTO cartProductUpdateRequestDTO) {
 		cartPersistence.modifyCartProduct(cartProductUpdateRequestDTO);
 	}
-
+	
 	@Transactional(readOnly = false)
 	public void removeCart(@NonNull CartProductDeleteRequestDTO cartProductDeleteRequestDTO) {
 		cartPersistence.deleteCart(cartProductDeleteRequestDTO);
