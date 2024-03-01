40e8173c9caeae6ec503015188bb86475594fbd3
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-03-01 21:18:06 +0900

    refactor: 수정 서비스 로직 변경 및 삭제 구문 퍼시스턴스로 이동

--- src/main/java/com/psj/itembrowser/cart/service/CartService.java
+++ src/main/java/com/psj/itembrowser/cart/service/CartService.java
@@ -1,5 +1,10 @@
 package com.psj.itembrowser.cart.service;
 
+import java.util.Objects;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.psj.itembrowser.cart.domain.dto.request.CartProductDeleteRequestDTO;
 import com.psj.itembrowser.cart.domain.dto.request.CartProductRequestDTO;
 import com.psj.itembrowser.cart.domain.dto.request.CartProductUpdateRequestDTO;
@@ -13,13 +18,10 @@ import com.psj.itembrowser.product.service.ProductValidationHelper;
 import com.psj.itembrowser.security.common.exception.ErrorCode;
 import com.psj.itembrowser.security.common.exception.NotAuthorizedException;
 import com.psj.itembrowser.security.common.exception.NotFoundException;
+
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
 
 /**
  * packageName    : com.psj.itembrowser.test.service.impl fileName       : TestServiceImpl author
@@ -33,99 +35,93 @@ import java.util.Objects;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CartService {
-    private final CartProductRelationEntityRepository cartProductRelationEntityRepository;
-    
-    private final CartPersistence cartPersistence;
-    
-    private final ProductValidationHelper productValidationHelper;
-    
-    public CartResponseDTO getCart(String userEmail) {
-        return cartPersistence.getCart(userEmail);
-    }
-    
-    public CartResponseDTO getCart(Long cartId) {
-        return cartPersistence.getCart(cartId);
-    }
-    
-    @Transactional(readOnly = false)
-    public CartResponseDTO createCart(@NonNull String userId) {
-        return cartPersistence.createCart(userId);
-    }
-    
-    @Transactional(readOnly = false)
-    public void addCartProduct(MemberEntity member, CartProductRequestDTO requestDTO) {
-        validateMemberAuth(member, requestDTO);
-        
-        productValidationHelper.validateProduct(requestDTO.getProductId());
-        
-        CartResponseDTO cart = null;
-        
-        try {
-            cart = getCart(requestDTO.getEmail());
-        } catch (NotFoundException e) {
-            log.info(e.getMessage());
-        }
-        
-        if (cart == null) {
-            cart = createCart(requestDTO.getEmail());
-            requestDTO.setCartId(cart.getCartId());
-        }
-        
-        CartProductRelationResponseDTO dto = null;
-        
-        try {
-            dto = cartPersistence.getCartProductRelation(cart.getCartId(), requestDTO.getProductId());
-        } catch (NotFoundException e) {
-            log.info(e.getMessage());
-        }
-        
-        if (dto == null) {
-            cartPersistence.addCartProductRelation(requestDTO);
-            return;
-        }
-        
-        CartProductRelationEntity foundCartProduct = CartProductRelationEntity.from(dto);
-        
-        foundCartProduct.addProductQuantity(requestDTO.getQuantity());
-        
-        cartProductRelationEntityRepository.save(foundCartProduct);
-    }
-    
-    private static void validateMemberAuth(MemberEntity member, CartProductRequestDTO requestDTO) {
-        if (!Objects.equals(member.getCredentials().getEmail(), requestDTO.getEmail())) {
-            throw new NotAuthorizedException(ErrorCode.CUSTOMER_NOT_AUTHORIZED);
-        }
-    }
-    
-    @Transactional(readOnly = false)
-    public void modifyCartProduct(CartProductUpdateRequestDTO cartProductUpdateRequestDTO, MemberEntity member) {
-        validateMemberAuth(member, cartProductUpdateRequestDTO.getCartId());
-        
-        cartPersistence.modifyCartProduct(cartProductUpdateRequestDTO);
-    }
-    
-    @Transactional(readOnly = false)
-    public void removeCartProduct(@NonNull CartProductDeleteRequestDTO cartProductDeleteRequestDTO, MemberEntity member) {
-        validateMemberAuth(member, cartProductDeleteRequestDTO.getCartId());
-        
-        CartProductRelationResponseDTO foundCartProductDTO = cartPersistence.getCartProductRelation(cartProductDeleteRequestDTO.getCartId(), cartProductDeleteRequestDTO.getProductId());
-        
-        CartProductRelationEntity foundCartProduct = CartProductRelationEntity.from(foundCartProductDTO);
-        
-        foundCartProduct.remove();
-        
-        cartProductRelationEntityRepository.save(foundCartProduct);
-    }
-    
-    private void validateMemberAuth(MemberEntity member, Long cartId) {
-        if (member.isAdmin()) {
-            return;
-        }
-        
-        CartResponseDTO found = cartPersistence.getCart(cartId);
-        
-        if (!Objects.equals(member.getCredentials().getEmail(), found.getUserEmail())) {
-            throw new NotAuthorizedException(ErrorCode.CUSTOMER_NOT_AUTHORIZED);
-        }
-    }
+	private final CartProductRelationEntityRepository cartProductRelationEntityRepository;
+	
+	private final CartPersistence cartPersistence;
+	
+	private final ProductValidationHelper productValidationHelper;
+	
+	public CartResponseDTO getCart(String userEmail) {
+		return cartPersistence.getCart(userEmail);
+	}
+	
+	public CartResponseDTO getCart(Long cartId) {
+		return cartPersistence.getCart(cartId);
+	}
+	
+	@Transactional(readOnly = false)
+	public CartResponseDTO createCart(@NonNull String userId) {
+		return cartPersistence.createCart(userId);
+	}
+	
+	@Transactional(readOnly = false)
+	public void addCartProduct(MemberEntity member, CartProductRequestDTO requestDTO) {
+		validateMemberAuth(member, requestDTO);
+		
+		productValidationHelper.validateProduct(requestDTO.getProductId());
+		
+		CartResponseDTO cart = null;
+		
+		try {
+			cart = getCart(requestDTO.getEmail());
+		} catch (NotFoundException e) {
+			log.info(e.getMessage());
+		}
+		
+		if (cart == null) {
+			cart = createCart(requestDTO.getEmail());
+			requestDTO.setCartId(cart.getCartId());
+		}
+		
+		CartProductRelationResponseDTO dto = null;
+		
+		try {
+			dto = cartPersistence.getCartProductRelation(cart.getCartId(), requestDTO.getProductId());
+		} catch (NotFoundException e) {
+			log.info(e.getMessage());
+		}
+		
+		if (dto == null) {
+			cartPersistence.addCartProductRelation(requestDTO);
+			return;
+		}
+		
+		CartProductRelationEntity foundCartProduct = CartProductRelationEntity.from(dto);
+		
+		foundCartProduct.addProductQuantity(requestDTO.getQuantity());
+		
+		cartProductRelationEntityRepository.save(foundCartProduct);
+	}
+	
+	private static void validateMemberAuth(MemberEntity member, CartProductRequestDTO requestDTO) {
+		if (!Objects.equals(member.getCredentials().getEmail(), requestDTO.getEmail())) {
+			throw new NotAuthorizedException(ErrorCode.CUSTOMER_NOT_AUTHORIZED);
+		}
+	}
+	
+	@Transactional(readOnly = false)
+	public void modifyCartProduct(CartProductUpdateRequestDTO cartProductUpdateRequestDTO, MemberEntity member) {
+		validateMemberAuth(member, cartProductUpdateRequestDTO.getCartId());
+		
+		cartPersistence.modifyCartProduct(cartProductUpdateRequestDTO);
+	}
+	
+	@Transactional(readOnly = false)
+	public void removeCartProduct(@NonNull CartProductDeleteRequestDTO cartProductDeleteRequestDTO, MemberEntity member) {
+		validateMemberAuth(member, cartProductDeleteRequestDTO.getCartId());
+		
+		cartPersistence.removeCartProduct(cartProductDeleteRequestDTO);
+	}
+	
+	private void validateMemberAuth(MemberEntity member, Long cartId) {
+		if (member.isAdmin()) {
+			return;
+		}
+		
+		CartResponseDTO found = cartPersistence.getCart(cartId);
+		
+		if (!Objects.equals(member.getCredentials().getEmail(), found.getUserEmail())) {
+			throw new NotAuthorizedException(ErrorCode.CUSTOMER_NOT_AUTHORIZED);
+		}
+	}
 }
