c0de809f411c019a71cba4023f06301b2ec61cea
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-03-08 17:32:27 +0900

    fix: 테스트코드 ID generation 관련 수정

--- src/test/java/com/psj/itembrowser/cart/service/CartInsertWithDBServiceTest.java
+++ src/test/java/com/psj/itembrowser/cart/service/CartInsertWithDBServiceTest.java
@@ -34,34 +34,34 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 @ServiceWithDBTest
 class CartInsertWithDBServiceTest {
-
+    
     @Autowired
     private TestEntityManager em;
-
+    
     @Autowired
     private CartRepository cartRepository;
-
+    
     @Autowired
     private CartProductRelationEntityRepository cartProductRelationEntityRepository;
-
+    
     @Mock
     private ProductValidationHelper productValidationHelper;
-
+    
     @Mock
     private CartMapper cartMapper;
-
+    
     private CartPersistence cartPersistence;
-
+    
     private CartService cartService;
-
+    
     private MemberEntity member;
     private CartProductRequestDTO requestDTO;
-
+    
     @BeforeEach
     void setUp() {
         cartPersistence = new CartPersistence(cartMapper, cartProductRelationEntityRepository, cartRepository);
         cartService = new CartService(cartProductRelationEntityRepository, cartPersistence, productValidationHelper);
-
+        
         member = MemberEntity.builder()
                 .address(
                         Address.builder()
@@ -85,9 +85,9 @@ class CartInsertWithDBServiceTest {
                 .role(Role.ROLE_CUSTOMER)
                 .status(Status.ACTIVE)
                 .build();
-
+        
         em.persist(member);
-
+        
         requestDTO = CartProductRequestDTO.builder()
                 .cartId(1L)
                 .email(member.getCredentials().getEmail())
@@ -95,59 +95,59 @@ class CartInsertWithDBServiceTest {
                 .quantity(1L)
                 .build();
     }
-
+    
     @Test
     @DisplayName("기존에 존재하는 장바구니가 없는 경우 새로운 장바구니를 생성하고, 해당 장바구니에 상품을 추가한다.")
     void When_AddCartProduct_Expect_CreateNewCartAndAddProduct() {
         //when
         cartService.addCartProduct(member, requestDTO);
-
+        
         //then
         CartEntity foundCart = cartRepository.findByUserEmail(member.getCredentials().getEmail()).get();
         CartProductRelationEntity foundCartProduct = em.find(CartProductRelationEntity.class,
                 new CartProductRelationEntity.CartProductRelationEntityId(foundCart.getId(), 1L));
-
+        
         assertThat(foundCart).isNotNull();
         assertThat(foundCart.getUserEmail()).isEqualTo(member.getCredentials().getEmail());
         assertThat(foundCartProduct.getProductQuantity()).isEqualTo(requestDTO.getQuantity());
     }
-
+    
     @Test
     @DisplayName("장바구니 요청과 실제 사용자의 이메일이 동일한지 체크한다.")
     void When_AddCartProduct_Expect_ValidateMemberAuth() {
         ReflectionTestUtils.setField(requestDTO, "email", "qkrtkdwnsFake@gmail.com");
-
+        
         //when
         assertThatThrownBy(() -> cartService.addCartProduct(member, requestDTO))
                 .isInstanceOf(NotAuthorizedException.class)
                 .hasMessageContaining(ErrorCode.CUSTOMER_NOT_AUTHORIZED.getMessage());
     }
-
+    
     @Test
     @DisplayName("장바구니는 존재하고, 해당 장바구니 상품이 존재하지 않는 경우 추가 테스트")
     void When_AddCartProduct_Expect_AddNewProduct() {
         CartEntity cart = CartEntity.builder()
                 .userEmail(member.getCredentials().getEmail())
                 .build();
-
+        
         em.persist(cart);
-
+        
         //when
         cartService.addCartProduct(member, requestDTO);
-
+        
         //then
         CartProductRelationEntity foundCartProduct = em.find(CartProductRelationEntity.class,
-                new CartProductRelationEntity.CartProductRelationEntityId(1L, 1L));
-
+                new CartProductRelationEntity.CartProductRelationEntityId(cart.getId(), 1L));
+        
         assertThat(foundCartProduct).isNotNull();
-
+        
         assertAll(
-                () -> assertThat(foundCartProduct.getCartId()).isEqualTo(1L),
+                () -> assertThat(foundCartProduct.getCartId()).isEqualTo(cart.getId()),
                 () -> assertThat(foundCartProduct.getProductId()).isEqualTo(1L),
                 () -> assertThat(foundCartProduct.getProductQuantity()).isEqualTo(1L)
         );
     }
-
+    
     @Test
     @DisplayName("장바구니는 존재하고, 해당 장바구니 상품이 존재하는 경우 수정이 올바르게 되는지 테스트")
     void When_AddCartProduct_Expect_UpdateProductQuantity() {
@@ -155,24 +155,24 @@ class CartInsertWithDBServiceTest {
         CartEntity cart = CartEntity.builder()
                 .userEmail(member.getCredentials().getEmail())
                 .build();
-
+        
         CartEntity savedCart = em.persist(cart);
-
+        
         CartProductRelationEntity cartProductRelation = CartProductRelationEntity.builder()
                 .cartId(savedCart.getId())
                 .productId(1L)
                 .productQuantity(1L)
                 .build();
-
+        
         em.persist(cartProductRelation);
-
+        
         //when
         cartService.addCartProduct(member, requestDTO);
-
+        
         //then
         CartProductRelationEntity foundCartProduct = em.find(CartProductRelationEntity.class,
                 new CartProductRelationEntity.CartProductRelationEntityId(savedCart.getId(), 1L));
-
+        
         assertThat(foundCartProduct).isNotNull();
         assertThat(foundCartProduct.getProductQuantity()).isEqualTo(2L);
     }
